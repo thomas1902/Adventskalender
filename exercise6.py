fb827d0ba3c83c8bb8121e0ccb0ac6ebf06116ac
thomas1902 <92606447+thomas1902@users.noreply.github.com>
2023-03-11 14:43:47 +0100

    Aufgabe6 Teil1 gelöst

---
+++ exercise6.py
@@ -0,0 +1,44 @@
+def tuningTrouble():
+    with open('exercise6input') as f:
+        ausgabe = f.readlines()
+        liste = []
+        counter = -1
+        differentChars = 0
+        index = 0
+        unicatelist = []
+
+        for sentence in ausgabe:
+            for character in sentence:
+                counter +=1
+                liste.append(character)
+                if len(liste) == 5:
+                    print(liste[0:-1],counter)
+                    samecounterListe = []
+                    TrueList = []
+                    for element in liste[0:-1]:
+                        samecounter = 0
+                        for secondelement in liste[0:-1]:
+                            if element == secondelement:
+                                samecounter = samecounter + 1
+                        samecounterListe.append(samecounter)
+                    for number in samecounterListe:
+                        if number == 1:
+                            TrueList.append(True)
+                        else : TrueList.append(False)
+                    if False not in TrueList:
+                        print(counter)
+                        break
+                    del (liste[0])
+
+def tuningTroublePartTwo():
+    print()
+
+
+
+
+
+
+
+
+
+tuningTrouble()
